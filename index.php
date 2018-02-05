03d5f8ec930ba9f106bb3180831a331714fcc596
sboyd <sboyd@csumb.edu>
2018-02-05 18:26:33 +0000

    files for class

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
-<?php
 
-?>
+<!DOCKTYPE HTML>
+<html>
+
+    <head>
+        <meta charset="utf-8"/>
+        <title>CST336 Main</title>
+        <style>
+            a{
+                text-decoration: none;
+                color: black;
+            }
+        </style>
+    </head>
+    
+    
+    <body>
+        <h1><u>Assignments</u></h1>
+        <a href="./Assignments/Homework 1/index.html">Homework 1</a>
+        
+        <hr/>
+        
+        
+        <h1><u>Labs</u></h1>
+        <a href="./Labs/PersonalPortfolio/index.html">Personal Portfolio</a>
+    </body>
+
+</html>
