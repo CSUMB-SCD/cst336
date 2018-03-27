1086c5c3dd1ba57927fe245ab5e817a1419a16a9
sboyd <sboyd@csumb.edu>
2018-03-27 06:09:54 +0000

    heroku update

--- Project/index.php
+++ Project/index.php
@@ -20,6 +20,20 @@
 <body>
     <div class="container">    
         <h1>Movies and Props Galore</h1>
+     
+      <form action="search.php" method="get" />
+        <label>
+            Search
+            <input type="text" name="keywords">
+        </label>
+       
+        <input type="submit" value="Search" />
+        
+        
+        
+        
+        
+        </form>
         <table>
             <form id = "formElements" action="index.php?show=true" method = "GET">
                 <tr>
@@ -28,10 +42,10 @@
                             <label>Year: </label>
                             <select name="YearList">
                                 <option value="none">Select</option>
-                                <option value=2018>2018</option>
-                                <option value=2017>2017</option>
-                                <option value=2016> 2016</option>
-                                <option value=2015>2015</option>
+                                <option value="2018">2018</option>
+                                <option value="2017">2017</option>
+                                <option value="2016"> 2016</option>
+                                <option value="2015">2015</option>
                             </select>
 
                         </div> 
@@ -46,7 +60,7 @@
                                 <option value="Drama">Drama</option>
                                 <option value="Fantasy">Fantasy</option>
                                 <option value="Science fiction">Science fiction</option>
-                                <option value="Thiller">Thiller</option>
+                                <option value="Thriller">Thriller</option>
                                 <option value="Adventure">Adventure</option>
                             </select>
 
@@ -85,13 +99,35 @@
                         $sql = "select movie.title, movie.description, movie.year, movie.genre, movie.poster_url, inventory.id, inventory.quantity, inventory.amount from movie inner join inventory_movie on movie.title = inventory_movie.title inner join inventory on inventory_movie.id = inventory.id;";
                     }
                     
-                    if(isset($_GET['GenreList']))
+                    if(isset($_GET['GenreList']) && isset($_GET['filter']))
                     {
-                        $sql = "select movie.title, movie.description, movie.year, movie.genre, movie.poster_url, inventory.id, inventory.quantity, inventory.amount from movie inner join inventory_movie on movie.title = inventory_movie.title inner join inventory on inventory_movie.id = inventory.id where movie.genre = " . $_GET['GenreList'] . ";";
+                        $sql = "select movie.title, movie.description, movie.year, movie.genre, movie.poster_url, inventory.id, inventory.quantity, inventory.amount from movie inner join inventory_movie on movie.title = inventory_movie.title inner join inventory on inventory_movie.id = inventory.id where movie.genre = " . "'" . $_GET['GenreList'] . "'" . ";";
                     }
-                    else if(isset($_GET['YearList']))
+                    else if(isset($_GET['YearList']) && isset($_GET['filter']))
                     {
                         $sql = "select movie.title, movie.description, movie.year, movie.genre, movie.poster_url, inventory.id, inventory.quantity, inventory.amount from movie inner join inventory_movie on movie.title = inventory_movie.title inner join inventory on inventory_movie.id = inventory.id where movie.year = " . $_GET['YearList'] . ";";
+                    }
+                    
+                    if(isset($_GET['GenreList']) && $_GET['sort'] == "ascending" && isset($_GET['filter']))
+                    {
+                        $sql = "select movie.title, movie.description, movie.year, movie.genre, movie.poster_url, inventory.id, inventory.quantity, inventory.amount from movie inner join inventory_movie on movie.title = inventory_movie.title inner join inventory on inventory_movie.id = inventory.id where movie.genre = " . "'" . $_GET['GenreList'] . "'" .  " order by movie.title ASC;";
+                    }
+                    
+                    
+                    if(isset($_GET['GenreList']) && $_GET['sort'] == "descending" && isset($_GET['filter']))
+                    {
+                        $sql = "select movie.title, movie.description, movie.year, movie.genre, movie.poster_url, inventory.id, inventory.quantity, inventory.amount from movie inner join inventory_movie on movie.title = inventory_movie.title inner join inventory on inventory_movie.id = inventory.id where movie.genre = " . "'" . $_GET['GenreList'] . "'". " order by movie.title DESC;";
+                    }
+                    
+                    
+                    if(isset($_GET['YearList']) && $_GET['sort'] == "ascending" && isset($_GET['filter']))
+                    {
+                        $sql = "select movie.title, movie.description, movie.year, movie.genre, movie.poster_url, inventory.id, inventory.quantity, inventory.amount from movie inner join inventory_movie on movie.title = inventory_movie.title inner join inventory on inventory_movie.id = inventory.id where movie.year = " . $_GET['YearList'] . " order by movie.title ASC;";
+                    }
+                    
+                    if(isset($_GET['YearList']) && $_GET['sort'] == "descending" && isset($_GET['filter']))
+                    {
+                        $sql = "select movie.title, movie.description, movie.year, movie.genre, movie.poster_url, inventory.id, inventory.quantity, inventory.amount from movie inner join inventory_movie on movie.title = inventory_movie.title inner join inventory on inventory_movie.id = inventory.id where movie.year = " . $_GET['YearList'] . " order by movie.title DESC;";
                     }
                     
                     $stmt = $dbConn->prepare($sql);
