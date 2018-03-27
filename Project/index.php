ad1d8fcf03fa7afa1322baea0a20bd20283bac61
sboyd <sboyd@csumb.edu>
2018-03-27 01:33:43 +0000

    heroku update

--- Project/index.php
+++ Project/index.php
@@ -93,7 +93,7 @@
                 <?php
                     for($i = 0; $i < count($result_movie); $i++)
                     {
-                        echo "<div style=display:inline;margin-left:10px;>";
+                        echo '<div style="display:inline;margin-left:10px;">';
                         echo"<img src= '" . $result_movie[$i][4] . "'height=300 width=200/>";
                         echo"<h3>" . $result_movie[$i][0] ."</h3>";
                         echo "</div>";
