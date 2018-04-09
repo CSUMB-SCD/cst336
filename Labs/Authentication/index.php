7795e47d120bf31c77ae249fb3240c98f3e3fe58
sboyd <sboyd@csumb.edu>
2018-04-09 19:48:10 +0000

    working on lab

--- Labs/Authentication/index.php
+++ Labs/Authentication/index.php
@@ -1,3 +1,42 @@
+<?php
+    $host = "127.0.0.1";
+    $dbname = "authentication";
+    $username = "root";
+    $password = 's3cr3t';
+    
+    
+    
+    try
+    {
+        $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        
+        if(isset($_POST['login']))
+        {
+            if(!empty($_POST['username']) &&  !empty($_POST['password']) && $_POST['username'] == "admin")
+            {
+                
+                // $page = "admin.php";
+                // header("Location: " . $page);
+                // exit;
+            }
+            else if(empty($_POST['username']) &&  empty($_POST['password'])) {
+                echo "Must enter username and password!";
+            }
+            else{
+                
+            }
+        }
+    }
+    catch(PDOException $e)
+    {
+        echo 'Connection failed: ' . $e->getMessage();
+    }
+    
+    
+    
+
+?>
+
 <!DOCKTYPE html>
 <html>
     <head>
@@ -6,6 +45,16 @@
     </head>
     
     <body>
-        
+        <form action="./index.php" method="POST">
+            Username
+            <input type="text" name="username">
+            <br>
+            Password
+            <input type="password" name="password">
+            <br>
+            <input type="submit" value="Login" name="login">
+        </form>
     </body>
 </html>
+
+
---
+++ Labs/Authentication/main.php
@@ -0,0 +1,31 @@
+<?php
+    $host = "127.0.0.1";
+    $dbname = "authentication";
+    $username = "root";
+    $password = 's3cr3t';
+    
+    
+    try
+    {
+        $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        
+    
+    }
+    catch(PDOException $e)
+    {
+        echo 'Connection failed: ' . $e->getMessage();
+    }
+
+?>
+
+
+<!DOCKTYPE html>
+<html>
+    <head>
+        <title>Authentication</title>
+    </head>
+    
+    <body>
+        
+    </body>
+</html>
